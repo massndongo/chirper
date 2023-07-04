cd79ab8ad8721ffbbda60081628ff6085a101342
massndongo86@gmail.com <massndongo86@gmail.com>
2023-07-04 20:32:06 +0000

    delete chirp

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store', 'edit', 'update'])
+    ->only(['index', 'store', 'edit', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
